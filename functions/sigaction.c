ba748803cdf0b1b6fd74c76fc92bf4ecf30b14c5
Shenya De Silva <shenya44@outlook.com>
2023-11-01 13:10:41 +0100

    Implementation of sigaction

--- functions/sigaction.c
+++ functions/sigaction.c
@@ -1,15 +1,32 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char **argv)
+void	sigint_handler(int signal)
 {
-	/*int n;
-	int numSecs;
-	sigset_t pendingMask;
-	sigset_t blockingMask;
-	sigset_t emptyMask;*/
+	if (signal == SIGINT)
+		printf("\n Intercepted SIGINT\n");
+}
 
-	printf("%s: PID is %ld\n", argv[0], (long)getpid());
+void set_signal_action(void)
+{
+	struct sigaction act;
+
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	//bzero(&act, sizeof(act));
+
+	act.sa_handler = &sigint_handler;
+	sigaction(SIGINT, &act, NULL);
+}
+
+
+int main(void)
+{
+	set_signal_action();
+
+	while (1)
+		pause();
 	return (0);
 }
