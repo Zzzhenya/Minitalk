f958a70e8309a0f79a8ec00392185606640f24d2
Shenya De Silva <shenya44@outlook.com>
2023-11-03 18:17:57 +0100

    Bithandler to handle bits and print letters

--- server.c
+++ server.c
@@ -1,31 +1,37 @@
 #include "minitalk.h"
 
+#include <stdio.h>
+
 t_data   message;
+
 /*
-void	bit_handler(int pid, char *str)
-{
 	take a stream of bits and convert it to a string of chars
-}
 */
 
-void	handler(int sig)
+void	bit_handler(int sig)
 {
-	if (sig ==SIGUSR1)
-		ft_printf("1\n");
-	else
-		ft_printf("0\n");
+	//ft_printf("step: %d", message.a);
+	message.a = message.a + ((sig & 1) << message.c);
+	message.c++;
+	if (message.c == 7)
+	{
+		ft_printf("%c", message.a);
+		if (!message.a)
+			ft_printf("\n");
+		message.a = 0;
+		message.c = 0;
+	}
 }
 
 int	main(void)
 {
 	struct sigaction act;
 
-
 	ft_printf ("server pid: %d\n", getpid());
 	sigemptyset(&act.sa_mask);
 	act.sa_flags = SA_SIGINFO;
-	act.sa_handler = &handler;
-	//signal(SIGUSR1, handler);
+	act.sa_handler = &bit_handler;
+	//signal(SIGUSR1, bit_handler);
 	sigaction(SIGUSR2, &act, NULL);
 	sigaction(SIGUSR1, &act, NULL);
 	while (1)
